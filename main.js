d8cdeda9f16b013141e8f8d77ce421bf106bd3bf
Felipe Dantas <felipetodantas@gmail.com>
2022-11-22 19:42:06 -0300

    add france vs australia result

--- main.js
+++ main.js
@@ -24,6 +24,32 @@ function createGame(op1, op2, group, team1, result, team2) {
    `
 }
 
+function createGroupScore() {
+   return `
+      <div class="group">
+         <ul>
+            <h2>Grupo A</h2>
+            <li>
+               <img src="./assets/icon-brazil.svg" alt="Bandeira Brasil">
+               <p>Brasil</p>
+            </li>
+            <li>
+               <img src="./assets/icon-argentina.svg" alt="Bandeira Argentina">
+               <p>Brasil</p>
+            </li>
+            <li>
+               <img src="./assets/icon-france.svg" alt="Bandeira França">
+               <p>Brasil</p>
+            </li>
+            <li>
+               <img src="./assets/icon-england.svg" alt="Bandeira Inglaterra">
+               <p>Brasil</p>
+            </li>
+         </ul>
+      </div>
+   `
+}
+
 function createStageH2(text) {
    delay += 0.4
    return `
@@ -43,9 +69,10 @@ function createDivider() {
 document.querySelector("#cards").innerHTML = 
    createDivider() +
    createStageH2("Fase de Grupos") +
+   // createGroupScore() +
    createCard("20/11", "domingo", createGame(0.3, 1, "A", "qatar", "0 x 2", "ecuador")) +
    createCard("21/11", "segunda", createGame(1 , 0.3, "B", "england", "6 x 2", "iran") + createGame(0.3, 1, "A", "senegal", "0 x 2", "netherlands") + createGame(0.3, 0.3, "B", "usa", "1 x 1", "wales")) +
-   createCard("22/11", "terça", createGame(0.3, 1, "C", "argentina", "1 x 2", "saudiarabia") + createGame(0.3, 0.3, "D", "denmark", "0 x 0", "tunisia") + createGame(0.3, 0.3, "C", "mexico", "0 x 0", "poland") + createGame(1, 1, "D", "france", "16:00", "australia")) +
+   createCard("22/11", "terça", createGame(0.3, 1, "C", "argentina", "1 x 2", "saudiarabia") + createGame(0.3, 0.3, "D", "denmark", "0 x 0", "tunisia") + createGame(0.3, 0.3, "C", "mexico", "0 x 0", "poland") + createGame(1, 0.3, "D", "france", "4 x 1", "australia")) +
    createCard("23/11", "quarta", createGame(1, 1, "F", "morroco", "07:00", "croatia") + createGame(1, 1, "E", "germany", "10:00", "japan") + createGame(1, 1, "E", "spain", "13:00", "costarica") + createGame(1, 1, "F", "belgium", "16:00", "canada")) +
    createCard("24/11", "quinta", createGame(1, 1, "G", "switzerland", "07:00", "cameroon") + createGame(1, 1, "H", "uruguai", "10:00", "southkorea") + createGame(1, 1, "H", "portugal", "13:00", "ghana") +createGame(1, 1, "G", "brazil", "16:00", "serbia")) +
    createCard("25/11", "sexta", createGame(1, 1, "B", "wales", "07:00", "iran") + createGame(1, 1, "A", "qatar", "10:00", "senegal") + createGame(1, 1, "A", "netherlands", "13:00", "ecuador") + createGame(1, 1, "B", "england", "16:00", "usa")) +
